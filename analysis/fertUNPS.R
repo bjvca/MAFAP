ca384b4d03bfd42a3971c1c608eacde3b8cd8baf
bjvca <bjorn.vancampenhout@gmail.com>
2017-04-19 17:14:36 +0200

    added non-parametric regression between yield and fertilizer use / extension

---
+++ analysis/fertUNPS.R
@@ -0,0 +1,117 @@
+library(foreign)
+##get fertilizer use - this is at plot level
+agsec3A2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC3A.dta")
+agsec3B2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC3B.dta")
+sum(duplicated(agsec3A2013[c("HHID","plotID")]))
+sum(duplicated(agsec3B2013[c("HHID","plotID")]))
+
+##merge in production - this is at crop level
+agsec5A2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC5A.dta")
+agsec5B2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC5B.dta")
+##get quantity at productionID level
+agsec5A2013$prod <- agsec5A2013$a5aq6a*agsec5A2013$a5aq6d
+agsec5B2013$prod <- agsec5B2013$a5bq6a*agsec5B2013$a5bq6d
+##aggregate to product level
+
+prodA2013 <- aggregate(agsec5A2013$prod, list(agsec5A2013$HHID, agsec5A2013$plotID, agsec5A2013$cropID), sum, na.rm=T)
+names(prodA2013) <- c("HHID","plotID","cropID","prod")
+prodB2013 <- aggregate(agsec5B2013$prod, list(agsec5B2013$HHID, agsec5B2013$plotID, agsec5B2013$cropID), sum, na.rm=T)
+names(prodB2013) <- c("HHID","plotID","cropID","prod")
+
+
+
+##merge in plot area - this is at crop level
+agsec4A2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC4A.dta")
+agsec4B2013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC4B.dta")
+agsec4B2013$a4bq9[agsec4B2013$a4bq8 == "Pure Stand"] <- 100
+
+##how to handle mixed cropping
+areaA2013 <- aggregate(cbind(agsec4A2013$a4aq7,agsec4A2013$a4aq9), list(agsec4A2013$HHID, agsec4A2013$plotID, agsec4A2013$cropID), sum, na.rm=T)
+names(areaA2013) <- c("HHID","plotID","cropID","area", "prop")
+
+
+areaB2013 <- aggregate(cbind(agsec4B2013$a4bq7,agsec4B2013$a4bq9), list(agsec4B2013$HHID, agsec4B2013$plotID, agsec4B2013$cropID), sum, na.rm=T)
+names(areaB2013) <- c("HHID","plotID","cropID","area", "prop")
+
+yieldA2013 <- merge(areaA2013, prodA2013, by = c("HHID","plotID","cropID"))
+yieldB2013 <- merge(areaB2013, prodB2013, by = c("HHID","plotID","cropID"))
+
+allA2013 <- merge(yieldA2013, agsec3A2013[c(1,3,17:23)])
+allB2013 <- merge(yieldB2013, agsec3B2013[c(1,2,17:23)])
+names(allA2013) <- c("HHID", "plotID", "cropID", "area", "prop", "prod", "fert_use", "fert_typ", "fert_qty", "fert_bgt", "fert_qty_bgt", "fert_paid", "fert_where_bgt")
+names(allB2013) <- c("HHID", "plotID", "cropID", "area", "prop", "prod", "fert_use", "fert_typ", "fert_qty", "fert_bgt", "fert_qty_bgt", "fert_paid", "fert_where_bgt")
+allA2013$season <- 1
+allB2013$season <- 2
+
+all2013 <- rbind(allA2013, allB2013)
+
+all2013$yield <- all2013$prod / (all2013$area * all2013$prop/100)
+all2013$yield[all2013$yield > 15000] <- NA 
+
+##merge in extension - this is at household level
+
+agsec92013 <- read.dta("/home/bjvca/data/projects/MAFAP/data/UNPS2013/AGSEC9.dta")
+agsec92013$agext <- 0
+agsec92013$agext[agsec92013$a9q5a=="Yes"] <- 1
+ext2013 <- aggregate(cbind(agsec92013$agext, agsec92013$wgt_X), list(agsec92013$HHID),max, na.rm=T)
+names(ext2013) <- c("HHID","ext","w")
+### calculate percentage of households that reported to have received extension on ag production in the last 12 months
+sum(ext2013$ext*ext2013$w)/sum(ext2013$w)
+
+all2013 <- merge(all2013, ext2013, by="HHID")
+
+library(locfit)
+
+#pdf("~/data/projects/MAFAP/analysis/yields.pdf",sep=""))
+
+
+fit <- locfit((fert_use == "Yes")~log(yield),data=all2013[  all2013$yield>0 & !is.na(all2013$yield),])
+fit0 <- scb(fit, ev = lfgrid(100))
+### this is an extremely inelegant solution - I can not extract the confidence bands from the scb object, I can only get them when I print the object to the screen.  So I decided to print the to disk and then load then again
+sink(file="temp.txt")
+print(fit0)
+sink()
+df <- read.table("temp.txt")
+df$fit <- (df$fit - mean(df$fit))/mean(df$fit)
+
+plot(df[,1],df$fit, type="l",  xlab="log(yield)", ylab="% change in proportion")
+#lines(df[,1],df$upper, lty=4)
+#lines(df[,1],df$lower, lty=4)
+
+fit <- locfit(ext~log(yield),data=all2013[  all2013$yield>0 & !is.na(all2013$yield),])
+fit0 <- scb(fit, ev = lfgrid(100))
+### this is an extremely inelegant solution - I can not extract the confidence bands from the scb object, I can only get them when I print the object to the screen.  So I decided to print the to disk and then load then again
+sink(file="temp.txt")
+print(fit0)
+sink()
+df <- read.table("temp.txt")
+df$fit <- (df$fit - mean(df$fit))/mean(df$fit)
+
+#plot(df[,1],df$fit, type="l",  xlab="log(total land)", ylab="probability", xlim=c(5.5,7.5))
+lines(df[,1],df$fit, lty=4, col="red")
+#lines(df[,1],df$lower, lty=4)
+#text(-0.5, 0.1, "commercialized")
+
+fit <- locfit((fert_use == "Yes")*ext~log(yield),data=all2013[  all2013$yield>0 & !is.na(all2013$yield),])
+fit0 <- scb(fit, ev = lfgrid(100))
+### this is an extremely inelegant solution - I can not extract the confidence bands from the scb object, I can only get them when I print the object to the screen.  So I decided to print the to disk and then load then again
+sink(file="temp.txt")
+print(fit0)
+sink()
+df <- read.table("temp.txt")
+df$fit <- (df$fit - mean(df$fit))/mean(df$fit)
+#plot(df[,1],df$fit, type="l",  xlab="log(total land)", ylab="probability", xlim=c(5.5,7.5))
+lines(df[,1],df$fit, lty=4, col="green")
+#lines(df[,1],df$lower, lty=4)
+#text(-0.5, 0.1, "commercialized")
+
+ quantile(log(all2013$yield[all2]), na.rm=T)
+
+
+
+
+
+
+
+
+
